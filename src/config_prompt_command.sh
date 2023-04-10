b09e121823aaf88ff1e6b18ebac6d039f8d1c8ac
Antonio Abella <aabel011@gmail.com>
2023-04-10 00:00:00 -0400

    prompt emit eval strings

--- src/config_prompt_command.sh
+++ src/config_prompt_command.sh
@@ -1,4 +1,13 @@
-echo "# this file is located in 'src/config_prompt_command.sh'"
-echo "# code for 'hive config prompt' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+# echo "# this file is located in 'src/config_prompt_command.sh'"
+# echo "# code for 'hive config prompt' goes here"
+# echo "# you can edit it freely and regenerate (it will not be overwritten)"
+# inspect_args
+
+case "${args[TOGGLE]}" in
+  enable)
+    echo "export HIVE_OLD_PS1=\"\$PS1\"; PS1=\"(\$($(realpath -s "$0") config cluster show -f name)) \$PS1\""
+  ;;
+  disable)
+    echo "PS1=\"\$HIVE_OLD_PS1\"; unset HIVE_OLD_PS1"
+  ;;
+esac
